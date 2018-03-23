ecbef95e2a70ed0a7577421b6e7601018bb76f22
Cturner013 <cturner7@live.maryville.edu>
2018-03-22 20:34:17 -0400

    About Me

--- src/AboutYou.java
+++ src/AboutYou.java
@@ -4,10 +4,10 @@ public class AboutYou {
 	public static void main(String[] args) {
 		// TODO Modify the values in the String literals in ALL_CAPS below
 		System.out.print("Hello, my name is ");
-		System.out.println("YOUR_NAME");
+		System.out.println("Carolyn Turner");
 
 		System.out.print("And I'm taking this class because ");
-		System.out.println("BECAUSE_WHY");
+		System.out.println("This class is part of my degree. However, I've been really wanting to learn Java!");
 	}
 
 }
